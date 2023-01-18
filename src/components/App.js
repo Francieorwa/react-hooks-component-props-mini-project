24c67d3b1b01c31cda3df6b006a309fef7a04d40
Francisca <francisca.orwa@student.moringaschool.com>
2023-01-18 14:09:49 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 import blogData from "../data/blog";
+import About from "./About";
+import Header from "./Header";
+import Article from "./Article";
+import ArticleList from "../components/ArticleList";
 
 console.log(blogData);
 
@@ -8,6 +12,11 @@ function App() {
     <div className="App">
       You're on your own from here! Follow the deliverables; test things out in
       the browser as you write your code; and good luck!
+
+      <Header  image = {blogData.image}/>
+      <About  img = {blogData.img}  about = {blogData.about} />
+      <Article />
+      <ArticleList />
     </div>
   );
 }
